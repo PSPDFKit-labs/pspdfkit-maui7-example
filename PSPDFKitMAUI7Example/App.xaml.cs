5be2b2ae1b5c778ed37e165644467d7fb3ffacc4
Patrik Simic <patrik.simic@pspdfkit.com>
2022-10-12 19:10:28 +0200

    init

---
+++ PSPDFKitMAUI7Example/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PSPDFKitMAUI7Example;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ PSPDFKitMAUI7Example/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PSPDFKitMAUI7Example;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
