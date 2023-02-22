cc09379c0ce305afe03486c8b374a288a6393d39
Patrik Simic <patrik.simic@gmail.com>
2023-02-22 16:51:44 +0100

    adding permissions
    adding permissions for opening a document from the device - like in version 6

--- PSPDFKitMAUI7Example/Platforms/Android/MainActivity.cs
+++ PSPDFKitMAUI7Example/Platforms/Android/MainActivity.cs
@@ -1,10 +1,18 @@
-﻿using Android.App;
+using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.App;
 
 namespace PSPDFKitMAUI7Example;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        ActivityCompat.RequestPermissions(this, new[] { Manifest.Permission.AccessNetworkState, Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage }, 0);
+    }
 }
